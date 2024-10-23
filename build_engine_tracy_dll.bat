8e239338bde12e7702f39965b861f8a9c422bf96
RonYanDaik <andronakiy@gmail.com>
2024-10-23 22:56:00 +0300

    submodule del

--- build_engine_tracy_dll.bat
+++ build_engine_tracy_dll.bat
@@ -1,1 +1,1 @@
-scons platform=windows tracy_enable=true CCFLAGS="-fno-omit-frame-pointer -fno-inline -ggdb3" extra_suffix="tracy_dll"
+scons platform=windows keyboardhook_enable=true tracy_enable=true CCFLAGS="-fno-omit-frame-pointer -fno-inline -ggdb3" extra_suffix="tracy_dll"
