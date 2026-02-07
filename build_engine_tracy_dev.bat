29c27176e138845246e5f49b818582d6f3e900e2
RonYanDaik <andronakiy@gmail.com>
2026-02-07 15:39:40 +0200

    1

--- build_engine_tracy_dev.bat
+++ build_engine_tracy_dev.bat
@@ -1,1 +1,1 @@
-scons platform=windows keyboardhook_enable=true dev_build=yes debug_symbols=yes tracy_enable=yes CCFLAGS="-fno-omit-frame-pointer -fno-inline -ggdb3" extra_suffix="tracy"
+scons platform=windows keyboardhook_enable=true dev_build=yes debug_symbols=yes profiler=tracy profiler_path=".modules/godot_tracy_dll/tracy" CCFLAGS="-fno-omit-frame-pointer -fno-inline -ggdb3" extra_suffix="tracy"
